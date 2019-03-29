b3605f388b572e54ebb6946d7cd3bb021a62ed24
devzhou <zhouyu@devzhou.cn>
2019-03-29 12:30:19 +0800

    Add Code

---
+++ WebAplication/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace WebAplication.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+        }
+        
+        [HttpGet]
+        [Route("/")]
+        public string Index()
+        {
+            var now = DateTimeOffset.Now.LocalDateTime.ToString("yyyy-MM-dd hh:mm:ss");
+            var version = _configuration["Version"];
+            return $"Now:{now}  Application Version:{version}";
+        }
+    }
+}
--- WebAplication/Program.cs
+++ WebAplication/Program.cs
@@ -19,6 +19,8 @@ namespace WebAplication
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseUrls("http://*:80")
                 .UseStartup<Startup>();
     }
 }
